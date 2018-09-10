462486f76c4eececfe5ef1d6058e3d8e4892b838
John Coumbe <john.coumbe@gmail.com>
2018-09-10 08:10:19 +0100

    Tweak temps

--- starter/_javascript/es6-summary/code.js
+++ starter/_javascript/es6-summary/code.js
@@ -10,12 +10,12 @@
 // ES6 modules
 
 let spain = [
-	{ image:"sevilla.jpg", name:"Sevilla", region:"Andalusia", popl:1.2, temp:77,  },
-	{ image:"valencia.jpg", name:"Valencia", region:"Valencia", popl:1.5, temp:70,  },
-	{ image:"barcelona.jpg", name:"Barcelona", region:"Catalonia", popl:4.0, temp:72,  },
-	{ image:"granada.jpg", name:"Granada", region:"Andalusia", popl:1.25, temp:72,  },
-	{ image:"caceres.jpg", name:"Caceres", region:"Extremadura", popl:0.1, temp:76 },
-	{ image:"alicante.jpg", name:"Alicante", region:"Valencia", popl:0.7, temp:68 },
+	{ image:"sevilla.jpg", name:"Sevilla", region:"Andalusia", popl:1.2, temp:104,  },
+	{ image:"valencia.jpg", name:"Valencia", region:"Valencia", popl:1.5, temp:88,  },
+	{ image:"barcelona.jpg", name:"Barcelona", region:"Catalonia", popl:4.0, temp:76,  },
+	{ image:"granada.jpg", name:"Granada", region:"Andalusia", popl:1.25, temp:84,  },
+	{ image:"caceres.jpg", name:"Caceres", region:"Extremadura", popl:0.1, temp:94 },
+	{ image:"alicante.jpg", name:"Alicante", region:"Valencia", popl:0.7, temp:72 },
 	{ image:"zaragoza.jpg", name:"Zaragoza", region:"Aragon", popl:0.7, temp:64 },
 	{ image:"vigo.jpg", name:"Vigo", region:"Galician", popl:0.2, temp:60 }
 ];
